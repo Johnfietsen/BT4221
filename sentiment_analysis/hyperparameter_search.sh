90dffeba7fc4371f39b0dd71337750fe534e53b8
Johnfietsen <lucstefelmanns@live.nl>
2019-10-24 15:36:24 +0000

    hyperparameter search results non-base model

--- sentiment_analysis/hyperparameter_search.sh
+++ sentiment_analysis/hyperparameter_search.sh
@@ -1,8 +1,11 @@
+for file_name in "sanders.csv" "semeval_balanced.csv" "semeval_sanders.csv"
+do
 for i in 64 128 256
 do
 for j in 64 128 256
 do
-echo embed_dim=$i lstm_out=$j
-python3 lstm.py --embed_dim=$i --lstm_out=$j
+echo $file_name embed_dim=$i lstm_out=$j 
+python3 lstm.py --embed_dim=$i --lstm_out=$j --pretrain=True --data_file=$file_name --pretrained_folder=${i}_${j}
+done
 done
 done
